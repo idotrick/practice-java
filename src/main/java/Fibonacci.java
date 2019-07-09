a2e37ed53742aaf557371baf6053c863fb13d3cf
Damith <dwanninayaka@gmail.com>
2019-07-09 17:01:30 +0800

    Improved Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -10,6 +10,8 @@ import java.util.*;
 
 public class Fibonacci {
 
+  private static Map<Integer, Long> fibCache = new TreeMap<>();
+
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.print("Print Fibonacci Series upto: ");
@@ -18,36 +20,55 @@ public class Fibonacci {
   }
 
   public static void printFibSeriesUpTo(int n) {
-    List<Integer> fibSeries = new LinkedList<>();
-
-    System.out.println("Printing Fibonacci Series using recursion upto " + n);
-    for(int i=1;i<=n;i++) {
-      fibSeries.add(fibonacciRecursion(i));
-    }
-    System.out.println(fibSeries);
-    fibSeries.clear();
+    List<Long> fibSeries = new LinkedList<>();
 
     System.out.println("Printing Fibonacci Series using iteration upto " + n);
     for(int i=1;i<=n;i++) {
       fibSeries.add(fibonacciIteration(i));
     }
     System.out.println(fibSeries);
+
+    System.out.println("Printing Fibonacci Series using improved recursion upto " + n);
+    fibCache.put(1,Long.valueOf(1));
+    fibCache.put(2,Long.valueOf(1));
+    fibCache.put(n,fibonacciRecursionImproved(n));
+
+    System.out.println(fibCache.values());
+
+    System.out.println("Printing Fibonacci Series using recursion upto " + n);
+    fibSeries.clear();
+    for(int i=1;i<=n;i++) {
+      fibSeries.add(fibonacciRecursion(i));
+    }
+    System.out.println(fibSeries);
   }
 
   // Return nth fibonacci using tail recursion
-  public static int fibonacciRecursion(int n) {
+  public static long fibonacciRecursion(int n) {
     if(n==1 || n==2)
-      return 1;
+      return Long.valueOf(1);
     else
       return fibonacciRecursion(n-1) + fibonacciRecursion(n-2);
   }
 
-  // Return nth fibonacci using iteration
-  public static int fibonacciIteration(int n) {
+  // Return nth fibonacci using tail recursion - improved
+  public static long fibonacciRecursionImproved(int n) {
     if(n==1 || n==2)
-      return 1;
+      return Long.valueOf(1);
 
-    int fib1=1, fib2=1, fibonacci=-1;
+    if(!fibCache.containsKey(n)) {
+      fibCache.put(n, (fibonacciRecursionImproved(n - 1) + fibonacciRecursionImproved(n - 2)));
+    }
+
+    return fibCache.get(n);
+  }
+
+  // Return nth fibonacci using iteration
+  public static long fibonacciIteration(int n) {
+    if(n==1 || n==2)
+      return Long.valueOf(1);
+
+    Long fib1=Long.valueOf(1), fib2=Long.valueOf(1), fibonacci=Long.valueOf(-1);
 
     for(int i=3;i<=n;i++) {
       fibonacci = fib1 + fib2;
