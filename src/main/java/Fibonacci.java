3f06697866ebcebd9ab864efc11a0594fb3acf16
Damith <dwanninayaka@gmail.com>
2019-07-09 12:22:28 +0800

    Modify

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -11,7 +11,10 @@ import java.util.*;
 public class Fibonacci {
 
   public static void main(String args[]) {
-    printFibSeriesUpTo(5);
+    Scanner in = new Scanner(System.in);
+    System.out.print("Print Fibonacci Series upto: ");
+    int n = in.nextInt();
+    printFibSeriesUpTo(n);
   }
 
   public static void printFibSeriesUpTo(int n) {
