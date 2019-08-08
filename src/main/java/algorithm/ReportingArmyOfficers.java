b9be7f12511daa67d3bfcb3fce9d4ae808b5f669
Damith <dwanninayaka@gmail.com>
2019-08-08 11:39:26 +0800

    New practices

---
+++ src/main/java/algorithm/ReportingArmyOfficers.java
@@ -0,0 +1,39 @@
+/**
+ * Problem: Rank of army officers given in an array. Ex {3, 4, 3, 0, 2, 2, 3, 0, 0}. Any officer can report to a
+ * superior officer with exactly one rank above his rank. multiple officers can report to same superior officer.
+ * <p>
+ * Find the number of officers who have a superior officer. Ex Answer = 5. because (2, 2, 3, 3, 3) have superior officer.
+ *
+ * Solution: Complexity O(NLog(N))
+ */
+
+package algorithm;
+
+import java.util.*;
+
+public class ReportingArmyOfficers {
+  public int solution(int[] A) {
+    if (null == A || A.length < 2) {
+      return 0;
+    }
+
+    int reportingOfficerCnt = 0;
+    Set<Integer> officerCache = new HashSet<>();
+    Arrays.sort(A);
+
+    for (int i = 1; i < A.length; i++) {
+      int officer = A[i];
+      int superior = officer + 1;
+      if (officerCache.contains(superior)) {
+        ++reportingOfficerCnt;
+      } else {
+        if (Arrays.binarySearch(A, superior) >= 0) {
+          ++reportingOfficerCnt;
+          officerCache.add(superior);
+        }
+      }
+    }
+
+    return reportingOfficerCnt;
+  }
+}
---
+++ src/test/java/algorithm/ReportingArmyOfficersTest.java
@@ -0,0 +1,44 @@
+package algorithm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ReportingArmyOfficersTest {
+
+  private ReportingArmyOfficers testObject = new ReportingArmyOfficers();
+
+  @Test
+  void nullArray(){
+    int[] testCase = null;
+    int expected = 0;
+    int actual = testObject.solution(testCase);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void EmptyArray(){
+    int[] testCase = new int[0];
+    int expected = 0;
+    int actual = testObject.solution(testCase);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("TestCase: {1} expected: 0")
+  void onlyOneOfficer(){
+    int[] testCase = new int[]{1};
+    int expected = 0;
+    int actual = testObject.solution(testCase);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("TestCase: {3, 4, 3, 0, 2, 2, 3, 0, 0} expected: 5")
+  void validSmall(){
+    int[] testCase = new int[]{3, 4, 3, 0, 2, 2, 3, 0, 0};
+    int expected = 5;
+    int actual = testObject.solution(testCase);
+    Assertions.assertEquals(expected, actual);
+  }
+}
