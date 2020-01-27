670eb3eb77f26b5ee52c91c3d5cd57fbba1df3db
damith <dwanninayaka@gmail.com>
2020-01-27 22:35:46 +0800

    O(n) solution

--- src/main/java/futureskill/BuyAndSellGold.java
+++ src/main/java/futureskill/BuyAndSellGold.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -39,6 +41,22 @@ public class BuyAndSellGold {
         assertEquals(2,solution.getBuyDay());
         assertEquals(7,solution.getSellDay());
     }
+
+    @Test
+    void t5(){
+        API api = new API(List.of(12,8,22,4,10,8,12,11,16,10));
+        Solution solution = new Solution(api);
+        assertEquals(1,solution.getBuyDay());
+        assertEquals(2,solution.getSellDay());
+    }
+
+    @Test
+    void t6(){
+        API api = new API(List.of(12,8));
+        Solution solution = new Solution(api);
+        assertEquals(0,solution.getBuyDay());
+        assertEquals(1,solution.getSellDay());
+    }
 }
 
 interface SolutionInterface {
@@ -70,10 +88,13 @@ class Solution implements SolutionInterface {
         this.API=api;
         this.buyDay=0;
         this.sellDay=0;
-        getPrices();
+        getPricesGen2();
     }
 
-    void getPrices() {
+    /**
+     * Gen1 Solution with O(n^2) complexity
+     */
+    void getPricesGen1() {
         int numDays = API.getNumDays();
         int maxProfit = 0;
         for(int i=0;i<numDays;i++) {
@@ -90,6 +111,47 @@ class Solution implements SolutionInterface {
                 }
             }
         }
+    }
+
+    /**
+     * Gen2 solution with O(n) complexity
+     */
+    void getPricesGen2() {
+        int numDays = API.getNumDays();
+        int maxProfit = -1;
+        int temptedProfit = -1;
+        int buyPrice = 0;
+        int buyDay = -1;
+        int sellPrice = 0;
+        int sellDay = -1;
+
+        for(int i=0;i<numDays;i++) {
+            int todayPrice = API.getPriceOnDay(i);
+            if(i==0) {
+                buyPrice = todayPrice;
+                buyDay = i;
+            } else {
+                if (todayPrice >= buyPrice) {
+                    if (todayPrice > sellPrice) {
+                        sellPrice = todayPrice;
+                        sellDay = i;
+                    }
+                } else {
+                    buyPrice = todayPrice;
+                    buyDay = i;
+                    sellPrice = 0;
+                    sellDay = -1;
+                }
+            }
+
+            temptedProfit = sellPrice - buyPrice;
+            if(i == 0 || temptedProfit > maxProfit) {
+                maxProfit = temptedProfit;
+                this.buyDay = buyDay;
+                this.sellDay = sellDay;
+            }
+        }
+
     }
 
     public int getBuyDay() {
